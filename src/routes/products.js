56f1553897f71c4ebd6f1a3ab4b432622340e7f8
Iuri Magnago <iurimp91@gmail.com>
2021-06-29 20:57:34 -0300

    Filter products bt category and/or price

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,7 +2,21 @@ import db from "../dbConfig.js";
 
 export async function getProducts(req, res) {
     try {
-        const products = await db.query(`SELECT * FROM products WHERE 1 = 1`);
+        const { category, price } = req.query;
+
+        const categoryQuery = `AND "categoryId" = ${category}`;
+        const priceQuery = (
+            price === "maior" ? `ORDER BY price DESC` : `ORDER BY price`
+        ); 
+
+        const query = `
+            SELECT * FROM products
+            WHERE 1 = 1
+            ${category ? categoryQuery : ""}
+            ${price ? priceQuery : ""}
+        `;
+
+        const products = await db.query(query);
         res.send(products.rows);
     } catch (e) {
         console.log(e);
