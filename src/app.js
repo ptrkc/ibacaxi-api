ec17c650e029057475680518559ee0c29e816032
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-30 13:03:04 -0300

    Added GET /product/:id and tests

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { postSignIn, postSignUp, postSignOut } from "./routes/sign.js";
 import { getProducts } from "./routes/products.js";
+import { getProduct } from "./routes/product.js";
 
 const app = express();
 app.use(express.json());
@@ -14,5 +15,7 @@ app.post("/sign-up", (req, res) => postSignUp(req, res));
 app.post("/sign-out", (req, res) => postSignOut(req, res));
 
 app.get("/products", (req, res) => getProducts(req, res));
+
+app.get("/product/:id", (req, res) => getProduct(req, res));
 
 export default app;
