097e1cb3c54b5f059e47fe5558259453d3e3cb53
Iuri Magnago <iurimp91@gmail.com>
2021-06-30 18:05:49 -0300

    Categories route and tests implemented + category and price filter for products updated

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { postSignIn, postSignUp, postSignOut } from "./routes/sign.js";
 import { getProducts } from "./routes/products.js";
+import { getCategories } from "./routes/categories.js";
 
 const app = express();
 app.use(express.json());
@@ -14,5 +15,7 @@ app.post("/sign-up", (req, res) => postSignUp(req, res));
 app.post("/sign-out", (req, res) => postSignOut(req, res));
 
 app.get("/products", (req, res) => getProducts(req, res));
+
+app.get("/categories", (req, res) => getCategories(req, res));
 
 export default app;
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,23 +5,21 @@ export async function getProducts(req, res) {
         const { category, price } = req.query;
 
         let categoryQuery = "";
-        if(category.length > 1) {
-            categoryQuery = `AND "categoryId" = ${category[0]}`;
+        if(typeof(category) === "string") {
+            categoryQuery = `AND category = '${category}'`;
+        } else if (typeof(category) === "object") {
+            categoryQuery = `AND category = '${category[0]}'`;
             for(let i = 1; i < category.length; i++) {
-                categoryQuery += `OR "categoryId" = ${category[i]}`;
+                categoryQuery += `OR category = '${category[i]}'`;
             }
-        } else {
-            categoryQuery = `AND "categoryId" = ${category}`;
         }
         
         const priceQuery = (
-            price === "maior" ? `ORDER BY price DESC` : `ORDER BY price`
+            price === "higher" ? `ORDER BY price DESC` : `ORDER BY price`
         ); 
 
         const query = `
-            SELECT products.*, categories.name AS "categoryName"
-            FROM products JOIN categories
-            ON products."categoryId" = categories.id
+            SELECT * FROM products
             WHERE 1 = 1
             ${category ? categoryQuery : ""}
             ${price ? priceQuery : ""}
